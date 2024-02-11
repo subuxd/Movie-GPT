beca64907305a08f43b983a50a1103dc17ea66f7
subuxd <subhamksingh01@gmail.com>
2024-02-11 12:44:33 +0530

    SignIn, SignUp, Login, Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import "./App.css";
+import Body from "./components/Body";
 
 function App() {
-  return (
-    <div className="text-3xl font-bold text-green-400">
-      Hello everyone, welcome to my project Movie GPT
-    </div>
-  );
+  return <Body />;
 }
 
 export default App;
