181ab4fce9e6a5768a13b21fb32a8ff415d6f98e
subuxd <subhamksingh01@gmail.com>
2024-02-18 02:01:33 +0530

    fix

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -8,7 +8,7 @@ const MainContainer = () => {
 
   if (!movies) return;
 
-  const mainMovie = movies[12];
+  const mainMovie = movies[14];
   // console.log(mainMovie);
 
   const { title, overview, id } = mainMovie;
